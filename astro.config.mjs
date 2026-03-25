5ca84b01312e37366cb4c2bdf0cd037b8b154663
Jud Cole <judc@fulldev.com>
2026-03-24 21:42:16 -0500

    feat: add various home page meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,3 @@
-// @ts-check
-
 import svelte from "@astrojs/svelte";
 import starlight from "@astrojs/starlight";
 import { defineConfig } from "astro/config";
@@ -7,8 +5,10 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import starlightCatppuccin from "@catppuccin/starlight";
 
+// Configuration from https://docs.astro.build/en/reference/configuration-reference/
 export default defineConfig({
   site: "https://www.codehealergroup.com",
+  base: "/",
   image: {
     service: { entrypoint: "astro/assets/services/noop" },
   },
@@ -17,32 +17,62 @@ export default defineConfig({
     sitemap(),
     starlight({
       title: "Code Healer Group",
-      // social: [
-      //   {
-      //     label: "GitHub",
-      //     href: "https://github.com/withastro/starlight",
-      //     icon: "github",
-      //   },
-      // ],
+      components: {
+        Footer: "./src/components/Footer.astro",
+        Header: "./src/components/Header.astro",
+      },
+      customCss: ["./src/styles/global.css"],
       head: [
         {
           tag: "meta",
-          attrs: { property: "og:image", content: "https://www.codehealergroup.com/og.png" },
+          attrs: { name: "Robots", content: "all" },
+        },
+        {
+          tag: "meta",
+          attrs: {
+            name: "description",
+            content:
+              "Providing expert software architecture, development, security, management, and leadership consulting services to its clients since 2001",
+          },
+        },
+        {
+          tag: "meta",
+          attrs: {
+            name: "keywords",
+            content:
+              "ai architecture cole consulting contracting jud leadership rust security software",
+          },
+        },
+        {
+          tag: "meta",
+          attrs: { property: "og:description", content: "Code Healer Group website" },
+        },
+        {
+          tag: "meta",
+          attrs: {
+            property: "og:image",
+            content: "https://www.codehealergroup.com/images/chgroup_logo.gif",
+          },
+        },
+        {
+          tag: "meta",
+          attrs: { property: "og:type", content: "website" },
         },
       ],
-      customCss: ["./src/styles/global.css"],
       plugins: [
         starlightCatppuccin({
           dark: { flavor: "macchiato", accent: "mauve" },
           light: { flavor: "latte", accent: "mauve" },
         }),
       ],
-      sidebar: [
+      social: [
         {
-          label: "Guides",
-          autogenerate: { directory: "guides" },
+          label: "GitHub",
+          href: "https://github.com/judcole",
+          icon: "github",
         },
       ],
     }),
   ],
+  trailingSlash: "always",
 });
